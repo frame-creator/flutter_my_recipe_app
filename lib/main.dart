bd742c811e3466631b91b8a62bdaff0988914e5c
KOOYON <55563485+KOOYON@users.noreply.github.com>
2020-03-16 19:22:36 +0900

    코드 정리

--- lib/main.dart
+++ lib/main.dart
@@ -57,12 +57,7 @@ int _sideDuration = 2;
 
 
       
-    /*  floatingActionButton: FloatingActionButton(
-        onPressed: (){
-          setState(() {
-             _sideMoved = !_sideMoved ;
-            });
-          }) */
+    
         );
       
   }
@@ -180,73 +175,4 @@ int _sideDuration = 2;
    }
 
       
-      /* return Scaffold(
       
-      body: Center(
-        
-        child: Column(
-          
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              '홈 화면',
-            ),
-          ]
-        ),),);
-  }
-}
-
-*/
-/*
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    
-    return Scaffold(
-      appBar: AppBar(
-        
-        title: Text(widget.title),
-      ),
-      body: Center(
-        
-        child: Column(
-          
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), 
-    );
-  }
-} */
