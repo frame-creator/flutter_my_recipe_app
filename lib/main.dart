63b57cea00caecd4a5b05404372dd4773ae0c778
KOOYON <55563485+KOOYON@users.noreply.github.com>
2020-03-22 17:01:34 +0900

    탭바생성으로 인한 메인파일 변경

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:recipe_app/pages/tab_page.dart';
+
 
 void main() => runApp(MyApp());
 
@@ -14,12 +16,13 @@ class MyApp extends StatelessWidget {
         
         primarySwatch: Colors.yellow,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: TabPage(),
+      //MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-
+/*
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
@@ -57,7 +60,12 @@ int _sideDuration = 2;
 
 
       
-    
+    /*  floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          setState(() {
+             _sideMoved = !_sideMoved ;
+            });
+          }) */
         );
       
   }
@@ -173,6 +181,75 @@ int _sideDuration = 2;
      );
    } 
    }
+*/
+      
+      /* return Scaffold(
+      
+      body: Center(
+        
+        child: Column(
+          
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              '홈 화면',
+            ),
+          ]
+        ),),);
+  }
+}
 
+*/
+/*
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
       
-      
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return Scaffold(
+      appBar: AppBar(
+        
+        title: Text(widget.title),
+      ),
+      body: Center(
+        
+        child: Column(
+          
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.display1,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), 
+    );
+  }
+} */
